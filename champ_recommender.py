3d87146746e0657c9bed1ce04928052280a343bb
matthewh26 <115488877+matthewh26@users.noreply.github.com>
2024-04-14 00:17:37 +0100

    Add files via upload

--- champ_recommender.py
+++ champ_recommender.py
@@ -1,69 +1,65 @@
 import numpy as np
-from compnet import BinaryNN
+from win_predict import BinaryNN, Scaler
 import torch
 import pandas as pd
 
-matches = pd.read_csv('data/Lol_matchs.csv')
-columns = list(matches.columns)
-del columns[0]
-columns = [i.capitalize() for i in columns]
+df = pd.read_csv('match_data.csv')
+df = df.drop('win',axis=1)
+df = df.replace(r'\N', 0)
+df = df.apply(pd.to_numeric)
+champs = pd.read_csv('data/champs.csv')
+
+print('find out what champ best fits your comp!')
 
 
-print('find out what champ best fits your comp! \nSimply input the champs on your team and the enemy team \nseparated by commas')
-
-#initialize X to put through model and win probability array
-x = np.zeros(300)
-prob_win = np.zeros(150)
-
-#recieve team info
-team = input('input allied champs:')
-enemy_team = input('input enemy champs:')
-
-#create lists of champs on allied/enemy team 
-team_list = team.split(sep=',')
-team_list = [champ.strip() for champ in team_list]
-team_list = [i.capitalize() for i in team_list]
-enemy_list = enemy_team.split(sep=',')
-enemy_list = [champ.strip() for champ in enemy_list]
-enemy_list = [i.capitalize() for i in enemy_list]
+#recieve comp/role info
+#current iteration ONLY works for allied jungler
+role = input('input your role: (top, jungle, mid, duo_carry, duo_support)').upper()
+team_mate = input('current allied jungler:')
+team_mate = int(champs[champs['name']==team_mate]['id']) #turn team mate champ name to id
 
 #initialise model, load trained state
-NUM_FEATURES = 300
-HIDDEN_FEATURES = 20
-model = BinaryNN(NUM_FEATURES=NUM_FEATURES,HIDDEN_FEATURES=HIDDEN_FEATURES)
+model = BinaryNN()
 model.load_state_dict(torch.load('model_state_dict.pth'))
 
+#only data with team mate champ in game
+ddf = df[df['JUNGLE_blue_champ']==team_mate] 
 
-#change x to represent current allied/enemy comps
-for champ in team_list:
-    x[columns.index(champ)] = 1
-for champ in enemy_list:
-    x[columns.index(champ)+150] = 1
+#only include champs with enough data
+champs = ddf['TOP_blue_champ'].value_counts()
+top_champs = champs[champs>10].index 
 
+#aggregate by player's role mean values
+average_df_blue = ddf.groupby(['TOP_blue_champ']).mean()
+columns = [col for col in list(average_df_blue.columns) if 'TOP_blue' in col]
+average_df_blue = average_df_blue[columns].iloc[:,1:]
 
-#loop through all possible picks, generate win probability for each
-for i in range(150):
-    if x[i] != 1:
-        x[i] = 1
-        x_in = torch.from_numpy(x.astype('float32'))
+#find the predicted win prob from the model for each champ in player's role
+win_probs = []
+poss_champs = []
+for champ in top_champs:
+    row = average_df_blue[average_df_blue.index == champ]
+    row = row.to_numpy().astype('float32')
+    if row.shape[0] != 0:
+        row = Scaler.transform(row)
         with torch.no_grad():
-            y = model(x_in)
-        prob_win[i] = y
-        x[i] = 0
+            win_probs.append(model(torch.from_numpy(row)).item())
+            poss_champs.append(champ)
 
+#output champ id of top 10 recommended champs
+idxs = np.argsort(win_probs)
 
-#determine which champ is the best pick
-print('\n')
-print('your recommended pick is...')
-print(columns[np.argmax(prob_win)])
-print('win probability: ')
-print(prob_win[np.argmax(prob_win)])
-        
-
-
-#output pick recommendation to user
-print(prob_win)
+for i in range(10):
+    print(poss_champs[idxs[-i-1]])
+    print('win prob: ' + str(win_probs[idxs[-i-1]]))
 
 
 
-
+'''
+lots of improvement to be made here: 
+    - Only looks at blue side, can look at red side too by simply flipping the data and appending
+    - Not sure why I dont pass all of the average_df_blue through the model at the same time? QOL change
+    - Model currently uses average of all team's stats, this NEEDS to be changed to just the role of the user, e.g. just 
+    TOP stats for a top lane user are used to calc win prob
+    - Make it work for all teammates, not just allied jungler!
+'''
